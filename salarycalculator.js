18f11a9bfe83f504eece795450aa0e71f77bacb4
Sean Kimbrough <114960881+SeanKimbrough@users.noreply.github.com>
2023-02-26 15:36:23 -0600

    Got onSubmit Employee Function with 5 inputs, as well as html for addEmployee

--- salarycalculator.js
+++ salarycalculator.js
@@ -3,7 +3,7 @@ console.log('We are Ready to Rock and Roll');
 $(document).ready(onReady);
 
 let employees = [];
-let monthlyCosts= 0;
+let monthlyEmployeeCosts= 0;
 let totalBudget;
 
 function onReady() {
@@ -11,3 +11,11 @@ function onReady() {
     //listener for submit button
 }
 
+function onSubmitEmployee() {
+    let firstName= $('#first-name').val();
+    let lastName = $('#last-name').val();
+    let employeeID = $('#employee-idInput').val();
+    let jobTitle = $('#jobTitleInput') .val();
+    let annualSalary = $('#salaryInput').val();
+}
+console.log('It is working', firstName, lastName, employeeID, jobTitle, annualSalary);
