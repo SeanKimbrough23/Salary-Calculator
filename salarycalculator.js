faae4e3a653b347c5d07996192b7a38d647300a8
Sean Kimbrough <114960881+SeanKimbrough@users.noreply.github.com>
2023-02-26 17:53:58 -0600

    cannot find saved assignment with my progress, I have to submit one

--- salarycalculator.js
+++ salarycalculator.js
@@ -1,31 +1,43 @@
 console.log('We are Ready to Rock and Roll');
-
-$(document).ready(onReady);
+$(document).ready(readyNow);
 
 let employees = [];
 let monthlyEmployeeCosts= 0;
 let totalBudget;
 
-function onReady() {
-    $('#submitBrn').on('click' , onSubmitEmployee);
+function readyNow() {
+    $('#Submit').on('click', onSubmitEmployee);
     //listener for submit button
     $('#gridOfEmployees').on('click', '.delete-btn', deleteEmployee);
-}
+} 
+
+console.log('On Ready Working');
 function render () {
     // function to empty table and monthly employee costs
     $('.gridOfEmployees').empty();
-for (let i=0; i < employees.length; i++ ) {
-    $('.gridOfEmployees').append(`
-            <p class='light-gray-background' id='${i}'>${employees[i].firstName}</p>
-            <p class='baby-blue' id='${i}'>${employees[i].lastName}</p>
-            <p class='light-gray-background' id='${i}'>${employees[i].employeeID}</p>
-            <p class='baby-blue' id='${i}'>${employees[i].jobTitle}</p>
-            <p class='light-gray-background' id='${i}'>${employees[i].annualSalary}</p>
-            <button id='${i}' class='delete-btn'>Delete Employee</button>
+
+    function submitEmployee() {
+        let firstName = $('#first-name').val();
+        let lastName = $('#last-name').val();
+        let employeeID = $('#employee-id').val();
+        let jobTitle = $('#employee-title').val();
+        let annualSalary = $('#annual-salary').val();
+    
+for (let employee of employees) {
+    $('.salary-table').append(`
+    <tr>
+    <td>${employees.firstName}</td>
+    <td>${employees.lastName}</td>
+    <td>${employees.idNumber}</td>
+    <td>${employees.jobTitle}</td>
+    <td>${employees.annualSalary}</td>
+    <td>
+    <button class='deleteEmployeeBtn'></button>
+    </td>
         `);
 }
 }
-
+console.log("Render is working");
 
 function deleteEmployee () {
     console.log('Delete Employee');
@@ -33,43 +45,7 @@ function deleteEmployee () {
     employees.splice(indexOfEmployee, 1); // remove employee
     console.log('After Removal of employee' , employees);
     render ();
+
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function onSubmitEmployee() {
-    let firstName= $('#first-name').val();
-    let lastName = $('#last-name').val();
-    let employeeID = $('#employee-idInput').val();
-    let jobTitle = $('#jobTitleInput') .val();
-    let annualSalary = $('#salaryInput').val();
 }
-console.log('It is working', firstName, lastName, employeeID, jobTitle, annualSalary);
