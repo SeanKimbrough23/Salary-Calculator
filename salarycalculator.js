739eb61541d110574d8734703229d41f0d3d683a
Sean Kimbrough <114960881+SeanKimbrough@users.noreply.github.com>
2023-02-26 15:56:49 -0600

    added render function and added table for employees to be added into, along with delete button

--- salarycalculator.js
+++ salarycalculator.js
@@ -9,7 +9,61 @@ let totalBudget;
 function onReady() {
     $('#submitBrn').on('click' , onSubmitEmployee);
     //listener for submit button
+    $('#gridOfEmployees').on('click', '.delete-btn', deleteEmployee);
 }
+function render () {
+    // function to empty table and monthly employee costs
+    $('.gridOfEmployees').empty();
+for (let i=0; i < employees.length; i++ ) {
+    $('.gridOfEmployees').append(`
+            <p class='light-gray-background' id='${i}'>${employees[i].firstName}</p>
+            <p class='baby-blue' id='${i}'>${employees[i].lastName}</p>
+            <p class='light-gray-background' id='${i}'>${employees[i].employeeID}</p>
+            <p class='baby-blue' id='${i}'>${employees[i].jobTitle}</p>
+            <p class='light-gray-background' id='${i}'>${employees[i].annualSalary}</p>
+            <button id='${i}' class='delete-btn'>Delete Employee</button>
+        `);
+}
+}
+
+
+function deleteEmployee () {
+    console.log('Delete Employee');
+    let indexOfEmployee = this.id;
+    employees.splice(indexOfEmployee, 1); // remove employee
+    console.log('After Removal of employee' , employees);
+    render ();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function onSubmitEmployee() {
     let firstName= $('#first-name').val();
